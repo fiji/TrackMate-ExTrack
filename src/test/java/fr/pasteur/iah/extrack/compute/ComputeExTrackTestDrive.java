9a1304b0e4c18df77de9fec3a63a32b29f350aea
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2020-11-21 19:58:37 +0100

    Rework test drive for the computation GUI.

--- src/test/java/fr/pasteur/iah/extrack/compute/ComputeExTrackTestDrive.java
+++ src/test/java/fr/pasteur/iah/extrack/compute/ComputeExTrackTestDrive.java
@@ -2,7 +2,11 @@ package fr.pasteur.iah.extrack.compute;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Map;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import Jama.Matrix;
 import fiji.plugin.trackmate.Logger;
@@ -14,62 +18,21 @@ import fr.pasteur.iah.extrack.util.ExTrackUtil;
 
 public class ComputeExTrackTestDrive
 {
-	public static void main( final String[] args ) throws FileNotFoundException, IOException
+	public static void main( final String[] args ) throws FileNotFoundException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
 	{
+		Locale.setDefault( Locale.ROOT );
+		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
 
 		/*
 		 * Load data.
 		 */
 
-		final String trackFile = "samples/sim_tracks.npy";
+		final String trackFile = "samples/tracks.npy";
 		final Map< Integer, Matrix > tracks = NumPyReader.readTracks( trackFile );
 
 		/*
-		 * Neg likelihood for the Python implementation.
+		 * Launch GUI.
 		 */
-
-		final int frameLen = 8;
-		final int nbSubSteps = 1;
-		final boolean doFrame = true;
-		final boolean doPred = false;
-
-		/*
-		 * Optimize.
-		 */
-
-		final double localizationError = 0.20056507;
-		final double diffusionLength0 = 0.01048;
-		final double diffusionLength1 = 0.62981;
-		final double F0 = 0.06869082094;
-		final double probabilityOfUnbinding = 0.849915476;
-//			final double localizationError = 0.020056507;
-//			final double diffusionLength0 = 0.001048;
-//			final double diffusionLength1 = 0.062981;
-//			final double F0 = 0.6869082094;
-//			final double probabilityOfUnbinding = 0.0849915476;
-
-//		final TrackState trackstate = new TrackState(
-//				localizationError,
-//				diffusionLength0,
-//				diffusionLength1,
-//				F0,
-//				probabilityOfUnbinding,
-//				nbSubSteps,
-//				doFrame,
-//				frameLen,
-//				doPred );
-
-//			for ( final Integer id : tracks.keySet() )
-//			{
-//				final Matrix track = tracks.get( id );
-		//
-////				System.out.println( "Track " + id );
-////				track.print( 7, 4 );
-		//
-//				final double sumProba = trackstate.sumLogProbabilities( track );
-//				System.out.println( String.format( "%4d -> %12.8f", id, sumProba ) );
-//			}
-
 
 		final ExTrackComputeAction action = new ExTrackComputeAction();
 		action.setLogger( Logger.DEFAULT_LOGGER );
@@ -77,27 +40,5 @@ public class ComputeExTrackTestDrive
 		final Settings settings = new Settings();
 		final TrackMate trackmate = new TrackMate( model , settings  );
 		action.execute( trackmate );
-//		,
-//				localizationError,
-//				diffusionLength0,
-//				diffusionLength1,
-//				F0,
-//				probabilityOfUnbinding,
-//				nbSubSteps,
-//				doFrame,
-//				frameLen,
-//				doPred );
-
-//		action.execute(
-//				tracks,
-//				localizationError,
-//				diffusionLength0,
-//				diffusionLength1,
-//				F0,
-//				probabilityOfUnbinding,
-//				nbSubSteps,
-//				doFrame,
-//				frameLen,
-//				doPred );
 	}
 }
